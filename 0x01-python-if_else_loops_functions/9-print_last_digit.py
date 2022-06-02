36d4808a61ad27e2546a5f5a59e1295570cae103
codivily <codivily@gmail.com>
2022-06-02 04:06:04 -0700

    9-print_last_digit.py added

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    number = (repr(number)[-1])
+    number = repr(number)
+    number = (number[-1] if len(number) > 0 else "")
     print(number, end="")
     return (number)
