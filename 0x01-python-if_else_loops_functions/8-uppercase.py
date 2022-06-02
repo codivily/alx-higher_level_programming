5ea1799ca882851e10deed3989aef0be17e8362c
codivily <codivily@gmail.com>
2022-06-02 03:57:46 -0700

    8-uppercase.py added

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,9 @@ def uppercase(str):
     ordA = ord('A')
     ordz = ord('z')
     lastI = len(str) - 1
+    if lastI < 0:
+        print()
+        return
     for i in range(len(str)):
         c = ord(str[i])
         print('{:c}'.format(ordA + c - orda if (c >= orda and c <= ordz)
