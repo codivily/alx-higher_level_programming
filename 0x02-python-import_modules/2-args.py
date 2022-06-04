3f3380b9c06a22541a7a226cb8abfb610a2be313
codivily <codivily@gmail.com>
2022-06-04 04:40:37 -0700

    2-args.py fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     argc = len(sys.argv)
-    print("{:d} arguments{}".format(argc - 1, ("." if argc == 1 else ":")))
-    for i in range(1, argc)):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    text = "{:d} argument{:s}{:s}\n".format(
+        argc - 1, ("" if argc == 2 else "s"), ("." if argc == 1 else ":")
+    )
+    for i in range(1, argc):
+        text = "{:d}: {:s}\n".format(i, sys.argv[i])
+    print(text, end="")
