136154201508ccfb5d1e22ce1698369556a61693
codivily <codivily@gmail.com>
2022-06-04 04:07:24 -0700

    3-infinity_add.py added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     import sys
     argc = len(sys.argv)
     print("{:d} arguments{}".format(argc - 1, ("." if argc == 1 else ":")))
-    for i in range(1, len(sys.argv)):
+    for i in range(1, argc)):
         print("{:d}: {}".format(i, sys.argv[i]))
