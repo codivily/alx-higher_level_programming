b13b9186b24e3b64034fd860f590ad84cb2f6cb1
codivily <codivily@gmail.com>
2022-06-04 04:42:25 -0700

    2-args.py fix2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         argc - 1, ("" if argc == 2 else "s"), ("." if argc == 1 else ":")
     )
     for i in range(1, argc):
-        text = "{:d}: {:s}\n".format(i, sys.argv[i])
+        text += "{:d}: {:s}\n".format(i, sys.argv[i])
     print(text, end="")
