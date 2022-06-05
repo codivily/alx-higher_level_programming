ba6d7daf24c1665834bf26da0f60d1120e123aef
codivily <codivily@gmail.com>
2022-06-05 11:09:15 -0700

    3-prin_reversed_list_integer.py fix 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        raise Exception("Not a list")
     text = ""
     for e in reversed(my_list[:]):
         text += "{:d}\n".format(e)
