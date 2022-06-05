108460be5887d77575310006acbfc7884ec8356c
codivily <codivily@gmail.com>
2022-06-05 11:58:18 -0700

    7-add_tuple.py fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a_len = len(tuple_a)
     b_len = len(tuple_b)
-    max_len = (a_len if a_len > b_len else b_len)
     r = []
-    for i in range(0, max_len):
+    for i in range(0, 2):
         sum_ = 0
         if i < a_len:
             sum_ += tuple_a[i]
