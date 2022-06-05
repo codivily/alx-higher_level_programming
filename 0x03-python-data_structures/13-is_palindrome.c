731da940b1a48ee613ca36632d3f0b7d0e178493
codivily <codivily@gmail.com>
2022-06-05 16:34:28 -0700

    13-is_palindrome.c added

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * is_palindrome - checks if a singly list is a palindrome
+ * @head: the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *s = NULL, *e = NULL, *old_e = NULL;
+
+	if (!head || !*head)
+		return (1);
+	s = *head;
+	while (s != old_e)
+	{
+		e = s;
+		while (e->next != old_e)
+			e = e->next;
+		old_e = e;
+		if (s->n != e->n)
+			return (0);
+		s = s->next;
+	}
+
+	return (1);
+}
