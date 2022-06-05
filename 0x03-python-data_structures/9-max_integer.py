44eb05d4344d58298b3fe2528bfd7a0ac7a3930b
codivily <codivily@gmail.com>
2022-06-05 12:19:37 -0700

    9-max_integer.py fix indentation

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return
+    if not type(my_list) == list:
+        return None
     r = my_list[0]
     for e in my_list:
         if e > r:
-           r = e
+            r = e
     return (r)
