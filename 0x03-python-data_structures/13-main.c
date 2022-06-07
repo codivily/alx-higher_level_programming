7203aba05f6f826697503f1a0dda2621d9fee6cf
codivily <codivily@gmail.com>
2022-06-07 14:29:27 -0700

    13-is_palindrome.c fix 5

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -19,15 +19,16 @@ int main(void)
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 1972);
     add_nodeint_end(&head, 17);
-    /*add_nodeint_end(&head, 1);*/
+    add_nodeint_end(&head, 1);
     print_listint(head);
 
     if (is_palindrome(&head) == 1)
         printf("Linked list is a palindrome\n");
     else
         printf("Linked list is not a palindrome\n");
+   
 
     free_listint(head);
 
