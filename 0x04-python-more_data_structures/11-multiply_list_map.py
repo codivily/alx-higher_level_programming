4c22a3956b7d20f1b826464c41ae9ac7d8acd462
codivily <codivily@gmail.com>
2022-06-08 16:27:49 -0700

    11-multiply_list_map.py fix 2

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    return [x * number for x in my_list]
+    return [my_list[0] * number] + (my_list if not my_list else multiply_list_map(my_list[1:], number))
