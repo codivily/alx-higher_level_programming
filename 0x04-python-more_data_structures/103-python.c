188933fd136c9c7fd6185fa033e1bd93fc691e60
codivily <codivily@gmail.com>
2022-06-09 20:34:59 -0700

    103-python.c fix

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -45,7 +45,7 @@ void print_python_list(PyObject *p)
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", len);
-	printf("[*] Allocated %ld\n", ((PyListObject *) p)->allocated);
+	printf("[*] Allocated = %ld\n", ((PyListObject *) p)->allocated);
 	for (i = 0; i < len; i++)
 	{
 		tmp = PyList_GET_ITEM(p, i);
