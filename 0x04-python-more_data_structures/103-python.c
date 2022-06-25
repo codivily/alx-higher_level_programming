73bd971247271bffa1b49859405e808162b9281f
codivily <codivily@gmail.com>
2022-06-25 15:19:49 +0000

    103-python.c updated

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -20,8 +20,8 @@ void print_python_bytes(PyObject *p)
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		fprintf(stderr, "  x[ERROR] Invalid Bytes Object\n");
-		return;
+		fprintf(stderr, "  [ERROR] Invalid Bytes Object\n");
+		exit(-1);
 	}
 	sz = PyBytes_Size(p);
 	s = PyBytes_AsString(p);
