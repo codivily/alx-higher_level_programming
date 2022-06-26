751970ba54266100ea1e9b90050a4765da53f645
codivily <codivily@gmail.com>
2022-06-26 13:17:54 +0000

    103-magic_class.py added

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""The MagicClass module"""
+
+
+class MagicClass:
+    """The MagicClass for circle"""
+
+    def __init__(self, radius=0):
+        """Initialize the radius"""
+        if type(radius) is not in [int, float]:
+            raise TypeError('raise must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Return the area of the circle"""
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """Return the circumference of the circle"""
+        return 2 * math.pi * self.__radius
