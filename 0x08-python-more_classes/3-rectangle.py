ece9246e57454727459c63ebde15960ea56a2d9f
root <root@u-station>
2022-06-27 14:13:02 +0000

    3-rectangle added

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""This is the ``Rectangle`` module"""
+
+
+class Rectangle:
+    """The Rectangle class defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize an instance of the Rectangle class.
+
+            Args:
+                width (int): The width of the rectangle
+                height (int): The height of the rectangle
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the witdh of the rectangle"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle"""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Returns the permeter of the rectangle"""
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        """Returns a string represenation of a rectangle"""
+        text = ""
+        w = self.__width
+        h = self.__height
+
+        if h > 0:
+            text = "#" * w
+            h -= 1
+        return text + ("\n" + "#" * w) * h
