dc30a6108839eef59422dc4d80095fc9fb6f98e8
root <root@u-station>
2022-06-28 22:12:51 +0000

    101-located_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""This is the ``LockedClass`` module"""
+
+
 class LockedClass:
+    """The LockedClass"""
     def __setattr__(self, name, value):
+        """Set attribute only if name == 'first_name'"""
         if name != 'first_name':
             msg = "'LockedClass' object has no attribute '{}'"
             raise AttributeError(msg.format(name))
