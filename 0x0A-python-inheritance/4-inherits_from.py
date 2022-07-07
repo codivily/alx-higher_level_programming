1f8839391968e3028b3c1f845a28a51bbec23f47
root <root@u-station>
2022-07-07 19:40:56 +0000

    tests/1-my_list.txt updated

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,5 +7,4 @@ def inherits_from(obj, a_class):
         (directly or indirectly from the specified class)
         otherwise False
     """
-
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return type(obj) is not a_class and isinstance(obj, a_class)
