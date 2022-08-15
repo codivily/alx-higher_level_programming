061db659c48dc87cd3e30f4ae27b523ed5535463
codivily <codivily@gmail.com>
2022-08-15 15:50:03 +0000

    Updated: 9-cities.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- A script that lists all the cities contained in the database hbtn_0d_usa
-SELECT c.id as id, c.name as name, s.name as name
-FROM cities as c
-	LEFT JOIN states as s ON s.id = c.state_id;
+SELECT cities.id as id, cities.name as name, states.name as name
+FROM cities
+	JOIN states ON states.id = cities.state_id;
