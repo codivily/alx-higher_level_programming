6d4783b7d82260922061240daf6f2300f2460903
codivily <codivily@gmail.com>
2022-08-15 21:45:36 +0000

    Updated: 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- A script that lists all shows contained in hbtn_0d_tvshows database
 SELECT t.title, g.genre_id
-FROM tv_shows t INNER JOIN tv_show_genres g ON t.id = g.show_id;
+FROM tv_shows t INNER JOIN tv_show_genres g ON t.id = g.show_id ORDER BY t.title, g.genre_id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- A script that lists all the cities contained in the database hbtn_0d_usa
 SELECT c.id, c.name, s.name
-FROM cities AS c INNER JOIN states AS s ON s.id = c.state_id;
+FROM cities AS c LEFT  JOIN states AS s ON s.id = c.state_id;
