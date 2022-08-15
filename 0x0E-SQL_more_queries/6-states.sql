588d2e4efbe1c9b0889d52e47fd639f64d67b3c2
codivily <codivily@gmail.com>
2022-08-15 21:02:12 +0000

    Updated: 6-state.sql, 7-cities.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- A scripts that creats the database hbtn_0d_usa and the table `states`
-CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256));
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	CONSTRAINT uk_cities_id UNIQUE (id),
-	CONSTRAINT pk_cities (id),
+	CONSTRAINT pk_cities PRIMARY KEY (id),
 	CONSTRAINT fk_cities_states FOREIGN KEY (state_id) REFERENCES states(id));
