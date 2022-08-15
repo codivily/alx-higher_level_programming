6983e87dd3127883e5add684589fa287532d993e
codivily <codivily@gmail.com>
2022-08-15 14:36:44 +0000

    Updated: 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- A scripts that creats the database hbtn_0d_usa and the table `states`
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS htbn_0d_usa.states (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL);
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	CONSTRAINT pk_states PRIMARY KEY (id));
 
