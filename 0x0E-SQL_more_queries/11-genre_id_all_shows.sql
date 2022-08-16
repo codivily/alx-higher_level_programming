952c74f38d32e3927dd604b53501e256811b0223
codivily <codivily@gmail.com>
2022-08-16 12:33:03 +0000

    Updated: 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- A script that lists all shows contained in the database hbtn_0d_tvshows
-SELECT t.title, IFNULL(g.genre_id, 'NULL') as genre_id
+SELECT t.title, IF(ISNULL(g.genre_id), 'NULL', g.genre_id) as genre_id
 FROM tv_shows t INNER JOIN tv_show_genres g ON t.id = g.show_id 
 ORDER BY t.title, g.genre_id;
