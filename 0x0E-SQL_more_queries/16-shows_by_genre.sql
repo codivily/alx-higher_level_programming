31302ffadfea6a3b03a3444fa17e77252b28e5d2
codivily <codivily@gmail.com>
2022-08-16 13:58:05 +0000

    Created: 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- A script that lists all genres from hbtn_0d_tvshows, and displays the numb
+SELECT tv.title, genre.name
+FROM tv_shows tv
+	INNER JOIN tv_show_genres tvg
+		ON tvg.show_id = tv.id
+	INNER JOIN tv_genres genre
+		ON genre.id = tvg.genre_id
+ORDER BY tv.title, genre.name;
+
+
