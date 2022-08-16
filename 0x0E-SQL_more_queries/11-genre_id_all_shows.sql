b898e15aabf7959300f3f21bbb7170ed75bfaf23
codivily <codivily@gmail.com>
2022-08-16 12:39:46 +0000

    Fix: 11-genere_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- A script that lists all shows contained in hbtn_0d_tvshows database
 SELECT t.title, IF(ISNULL(g.genre_id) = 1, 'NULL', g.genre_id) AS genre_id
-FROM tv_shows t INNER JOIN tv_show_genres g ON t.id = g.show_id ORDER BY t.title, g.genre_id;
+FROM tv_shows t LEFT OUTER JOIN tv_show_genres g ON t.id = g.show_id ORDER BY t.title, g.genre_id;
