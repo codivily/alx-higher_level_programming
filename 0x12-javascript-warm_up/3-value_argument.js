9df8a1ea6c944dbe6f4bb9f17c62133ad3a05100
codivily <codivily@gmail.com>
2022-09-08 07:14:16 +0000

    3-value_argument.js updated

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,4 @@
 const process = require('node:process');
 const args = process.argv.slice(2);
 
-if (args[0] === undefined) {
-  console.log('No argument');
-} else {
-  args.forEach(arg => console.log(arg));
-}
+console.log(args[0] ? args[0] : 'No argument');
