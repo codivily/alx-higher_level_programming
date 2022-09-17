6b29b1e7bf4b2f0db1438811aac7aeadb3d7b339
codivily <codivily@gmail.com>
2022-09-17 11:06:29 +0000

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-let n = parseInt(args[0]);
-let r = 1;
 
-if (isNaN(n) || n === 0) {
-  console.log(1);
-} else {
-  while (n > 0) {
-    r *= n;
-    n -= 1;
+console.log(factorial(parseInt(args[0])));
+
+function factorial (n) {
+  if (isNaN(n) || n <= 0) {
+    return (1);
   }
-  console.log(r);
+  return (n * factorial(n - 1));
 }
