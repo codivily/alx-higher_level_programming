3fd1a9fc7f2c3f6ba687bd9fc56774b26bcbb4fb
codivily <codivily@gmail.com>
2022-09-17 11:30:24 +0000

    11-second_biggest.js fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(v => parseInt(v)).sort();
+const args = [...new Set(process.argv.slice(2)
+  .map(v => parseInt(v))
+  .sort((a, b) => (a - b)))
+];
+
 if (args.length <= 1) {
   console.log(0);
 } else {
