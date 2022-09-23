5e06860e67956cefdd64683aae41e25631e9762a
codivily <codivily@gmail.com>
2022-09-23 19:47:38 +0000

    6-post_email.py added

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter
+and displays the body of the resposne (decoded in utf-8)"""
+
+if __name__ == '__main__':
+    import sys
+    import request
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={'email': email})
+    print(r.text)
