7a52ed09ef6b7faf92479093fb4b93e9c79a997a
codivily <codivily@gmail.com>
2022-09-23 16:53:17 +0000

    6-peak.py updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,15 +6,9 @@ def find_peak(list_of_integers):
     """Finds a peak in alist of unsorted integers"""
     if not list_of_integers:
         return None
-    x = 1
     stop = len(list_of_integers) - 1
-    while x < stop:
-        left = list_of_integers[x - 1]
-        peak = list_of_integers[x]
-        right = list_of_integers[x + 1]
-
-        if left < peak and right < peak:
-            return peak
-        x += 1
-
-    return peak
+    for x in range(1, stop):
+        if list_of_integers[x] > list_of_integers[x - 1]:
+            if list_of_integers[x] > list_of_integers[x + 1]:
+                return list_of_integers[x]
+    return list_of_integers[x]
