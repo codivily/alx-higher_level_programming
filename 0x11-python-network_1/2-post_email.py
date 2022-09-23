e63c15a97afd2dbfb1d9e0d4173602b8f3483752
codivily <codivily@gmail.com>
2022-09-23 19:21:17 +0000

    2-post_email.py decoding updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     data = data.encode('ascii')
 
     with urllib.request.urlopen(url, data) as response:
-        print(response.read())
+        print(response.read().decode('utf-8'))
