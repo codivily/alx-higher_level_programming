b5590a856755d5d484d04400562659de6c4c66b4
codivily <codivily@gmail.com>
2022-09-23 19:17:40 +0000

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter
+and displays the body of the resposne (decoded in utf-8)"""
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('ascii')
+
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read())
