23c65123aa50c086a16c59e7dadbc439476b5a58
codivily <codivily@gmail.com>
2022-09-23 17:07:15 +0000

    100-status_code.sh fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that displays body size of a request using curl
-curl -s --write-out '%{size_download}\n' -o /dev/null "$1"
+curl -s --write-out '%{size_download}' -o /dev/null "$1"
