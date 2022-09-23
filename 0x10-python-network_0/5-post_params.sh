96deb7dad976f9080f33fa8721b0abad9c232571
codivily <codivily@gmail.com>
2022-09-23 15:59:02 +0000

    5-post_params.sh added

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A scripts that task in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -X POST -F "email=test@gmail.com" "subject=I will always be here for PLD" -s "$1"
