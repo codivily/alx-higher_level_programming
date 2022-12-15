ee13f7f11cc6b93d09085ca3570ee7473a8d2574
codivily <codivily@gmail.com>
2022-12-15 17:35:05 +0000

    1-body.sh updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Display the response body only if status code is 200
-curl -s "$1"
+response=$(curl -L -sw "%{http_code}" "$1")
+status_code="${response:${#response}-3}"
+
+if [[ "$status_code" -eq 200 ]]; then
+    echo "${response:0:${#response}-3}"
+fi
