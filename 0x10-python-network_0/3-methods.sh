99b053c7cace975a1c912fd9b77cd5a240e2261c
codivily <codivily@gmail.com>
2022-12-16 09:06:23 +0000

    3-methods.sh updated fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -I -L -s -X OPTIONS "$1" | head -n 2 | tail -n 1 | cut -d ":" -f 2 | sed 's/^ *//g'
+curl -I -L -s -X OPTIONS "$1" | grep -E "Allow:" | cut -d ":" -f 2 | sed 's/^ *//g'
