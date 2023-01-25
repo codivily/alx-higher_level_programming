8fdd52f631e173a6552f86f409b8bb221aba5860
codivily <codivily@gmail.com>
2023-01-25 22:24:04 +0000

    6-completed_tasks.js updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,21 +3,17 @@
 const request = require('request');
 const url = process.argv.slice(2)[0];
 
+if (url == null) process.exit(-1);
+
 request(url, (_, response, body) => {
-  if (response.statusCode !== 200) {
-    return;
-  }
+  if (response.statusCode !== 200) return;
   const todos = JSON.parse(body);
-
-  const users = {};
-  for (const todo of todos) {
-    if (users[todo.userId] === undefined) {
-      users[todo.userId] = 0;
-    }
-
+  const users = todos.reduce((users, todo) => {
     if (todo.completed) {
-      users[todo.userId] += 1;
+      if (users[todo.userId] == null) users[todo.userId] = 1;
+      else users[todo.userId] += 1;
     }
-  }
+    return users;
+  }, {});
   console.log(users);
 });
